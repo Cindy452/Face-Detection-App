b18a305213877527e00ef47c10f215f019b18d62
Your Name <you@example.com>
2021-01-18 18:16:27 -0600

    initial commit

---
+++ src/components/rank/Rank.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+         <div className='white f3'>
+        {'Cindy, your current entry count is...'}
+      </div>
+        </div>
+    )
+}
+
+export default Rank;
